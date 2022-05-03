15e13b2fe6c8be0c513bf8922fa26e06244c6982
Nitin Mishra <nitin.mishra@knoldus.com>
2022-05-03 12:46:19 +0530

    Adding new files

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# GIT CLONE SHELL
+# How to Use
+- git clone #this repo
+- cd GIT_CLONE_SHELL
+
+## Add an alias to script 
+- nano ~/.bashrc
+- ``` alias osc='cd #PATH_TO_REPOSITORY/git_clone_shell ; ./git_clone.sh' ```
+- Now ``` osc``` command can be executed from anywhere to run the script.
---
+++ git_clone.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Create git-contributions directory at home where all repositories would be cloned.
+if [ ! -d "$HOME"/git-contributions ]; then
+    mkdir "$HOME"/git-contributions
+fi
+
+cd "$HOME"/git-contributions || { printf "cd failed, exiting\n" >&2;  return 1; }
+
+# Clone repository using HTTPS web url
+printf "git HTTPS web url: "
+read -r gitcontributions
+git clone "$gitcontributions"
+echo $gitcontributions
+
+# Extract reponame 
+reponame="$(echo $gitcontributions | sed -r 's/.+\/([^.]+)(\.git)?/\1/')"
+echo "Repsoitory Name: $reponame"
+cd ~/git-contributions/$reponame
+
+# Create a new branch in cloned repository
+echo "Do you want to create a new branch?(y/n)"
+read -r ans
+if [ $ans == "Y" ] || [ $ans == "Yes" ] || [ $ans == "y" ] || [ $ans == "yes" ]; then
+    printf "Enter new branch name: "
+    read -r branch_name
+    git checkout -b "$branch_name"
+    echo "happy coding"
+elif [ $ans == "N" ] || [ $ans == "No" ] || [ $ans == "n" ] || [ $ans == "no" ]; then
+    echo "At master branch only"
+fi
+
+unset ans
+
